913c67ef70ab8bdea42e67d3b1d90a2b9d077a35
Thomas Eschemann <tom@eschemann.fr>
2014-01-05 18:52:17 +0100

    ajout dossier results vide

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-rm ./tests/results/*
+rm ./tests/results/*.dot
+rm ./tests/results/*.png
 for file in `find ./tests/*.p -type f -maxdepth 1 \( ! -iname ".*" \) | sed 's#.*/##'`
 do
     ./parser ./tests/$file > ./tests/results/$file.dot
